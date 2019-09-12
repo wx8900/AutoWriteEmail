966aaf4c83c3fd40b886220a8f6209633bf443e5
Jack Cai <cwx8900@gmail.com>
2019-09-12 16:25:49 +0800

    update

--- src/main/java/com/autowrite/test/AutoWriteController.java
+++ src/main/java/com/autowrite/test/AutoWriteController.java
@@ -35,9 +35,28 @@ public class AutoWriteController {
     private static StringBuffer sb = new StringBuffer(2048);
 
     static {
-        map.put("As", "As we discussed, ");
+        map.put("As", "As we discussed on the phone, ");
+        map.put("reference", "With reference to our telephone conversation, ");
         map.put("If", "If you need more information, please let me know. ");
-        map.put("forward", "I'm looking forward to your repay! ");
+        map.put("decision", "from our decision at the previous meeting, ");
+        map.put("confirm", "I am writing to confirm/enquire/inform you ");
+        map.put("follow", "I am writing to follow up on our earlier decision on the marketing campaign in Q2. ");
+        map.put("follow-up", "As a follow-up to our phone conversation yesterday, I wanted to get back to you about the pending issues of our agreement. ");
+        map.put("Attached", "Attached please find the draft product plan for your review and comment. ");
+        map.put("talk", "I want to talk to you over the phone regarding issues about report development and the project. ");
+        map.put("apologize", "We apologize for the delay and hope that it doesn't inconvenience you too much. ");
+        map.put("Congratulation", "Congratulations to all of you and thanks for your efforts. ");
+        map.put("convey", "Please convey my thanks to all the staff involved, they certainly did an excellent job. ");
+        map.put("Due", "Due to circumstances beyond our control, ");
+        map.put("appreciate", "I would really appreciate meeting up if you can spare the time. Please let me know what suits you best. ");
+        map.put("appreciated", "Any comments will be much appreciated. ");
+        map.put("forward", "Looking forward to receiving your comments in due course! ");
+        map.put("status", "Could you please let me know the status of this project? ");
+        map.put("support", "Please feel free to call me at any time, I will continually provide full support. ");
+        map.put("question", " If you’re any questions please let me know.");
+        map.put("respond","Once you respond to the above questions, we will decide which option we would like to pursue. ");
+        map.put("ASAP", "as soon as possible. ");
+        map.put("estimation", "Thank you and look forward to having your opinion on the estimation and schedule. ");
     }
 
     @RequestMapping("/customerInputWord")
